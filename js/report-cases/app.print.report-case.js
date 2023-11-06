56d35e6fdcd19999e20b7bef02db420b2a043e8b
Naim Sulejmani <naim.sulejmani@gmail.com>
2023-11-06 18:16:01 +0100

    added validation

--- js/report-cases/app.print.report-case.js
+++ js/report-cases/app.print.report-case.js
@@ -6,15 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(window.location.search);
         const rptCaseId = window.location.search.split("=")[1].trim();
         const apiReport = new ApiReportCase();
+
         const data = await apiReport.findById(rptCaseId);
 
-        reportCase.innerHTML = `
-        <p style = "${data.reporter==="Erblini" ? "color:red": "color:blue"}"><b>Raportuesi: </b> <i>${data.reporter}</i></p>
-        <p style="color:red"><b>Case Type: </b> <i>${data.caseType}</i></p>
-        <p><b>Data: </b> <i>${data.date}</i></p>
-        <p><b>Description: </b> <i>${data.description}</i></p>
-        <p><b>Address: </b> <i>${data.address}</i></p>
-        `;
+        console.log(data);
+
+        if (!data.status || data.status === 200)
+            reportCase.innerHTML = `
+                <p style = "${data.reporter === "Erblini" ? "color:red" : "color:blue"}"><b>Raportuesi: </b> <i>${data.reporter}</i></p>
+                <p style="color:red"><b>Case Type: </b> <i>${data.caseType}</i></p>
+                <p><b>Data: </b> <i>${data.date}</i></p>
+                <p><b>Description: </b> <i>${data.description}</i></p>
+                <p><b>Address: </b> <i>${data.address}</i></p>
+            `;
+        else
+            alert(data.message)
+
+
     }
 
     loadData();
