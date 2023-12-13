2c230675c9c8ec5ddc85a75f69fa3120d83abd3b
Naim Sulejmani <naim.sulejmani@gmail.com>
2023-12-13 19:51:31 +0100

    axios libary added and replaced api report case js

--- js/report-cases/api.report-case.js
+++ js/report-cases/api.report-case.js
@@ -2,54 +2,76 @@ class ApiReportCase {
     url = 'http://localhost:8080/api/report-cases';
 
     async findAll() {
-        const response = await fetch(this.url); // GET
-        return await response.json();
+        // const response = await fetch(this.url); // GET
+        // return await response.json();
+
+        // javascript ==
+
+        const response = await axios.get(this.url);
+        return response.status === 200 ? response.data : [];
+
     }
 
     async findById(reportCaseId) {
-        const response = await fetch(this.url + "/" + reportCaseId); // GET
-        return await response.json();
+        // const response = await fetch(this.url + "/" + reportCaseId); // GET
+        // return await response.json();
+
+        const response = await axios.get(this.url + "/" + reportCaseId);
+        return response.status === 200 ? response.data : {};
     }
 
     async deleteById(reportCaseId) {
-        const response = await fetch(this.url + "/" + reportCaseId, {
-            method: 'DELETE'
-        });
-        return await response.text();
+        // const response = await fetch(this.url + "/" + reportCaseId, {
+        //     method: 'DELETE'
+        // });
+        // return await response.text();
+
+        const response = await axios.delete(this.url + "/" + reportCaseId);
+        return response.status === 200 ? response.data : "NOT DELETED";
     }
 
 
     async register(reportCase) {
-        const response = await fetch(this.url, {
-            method: 'POST',
-            body: JSON.stringify(reportCase),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        return await response.text();
+        // const response = await fetch(this.url, {
+        //     method: 'POST',
+        //     body: JSON.stringify(reportCase),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // });
+        // return await response.text();
+
+        const response = await axios.post(this.url, reportCase);
+
+        return response.data;
     }
 
     async modify(reportCaseId, reportCase) {
-        const response = await fetch(this.url + '/' + reportCaseId, {
-            method: 'PUT',
-            body: JSON.stringify(reportCase),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        return await response.text();
+        // const response = await fetch(this.url + '/' + reportCaseId, {
+        //     method: 'PUT',
+        //     body: JSON.stringify(reportCase),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // });
+        // return await response.text();
+        const response = await axios.put(this.url + '/' + reportCaseId, reportCase);
+
+        return response.data;
     }
 
     async changeStatus(reportCaseId, reportCase) {
-        const response = await fetch(this.url + '/' + reportCaseId, {
-            method: 'PATCH',
-            body: JSON.stringify(reportCase),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        return await response.text();
+        // const response = await fetch(this.url + '/' + reportCaseId, {
+        //     method: 'PATCH',
+        //     body: JSON.stringify(reportCase),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // });
+        // return await response.text();
+        const response = await axios.patch(this.url + '/' + reportCaseId, reportCase);
+
+        return response.data;
     }
 }
 
