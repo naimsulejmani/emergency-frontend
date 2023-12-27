af0ba994babe20454cc76e045592022ffeadbc9e
Naim Sulejmani <naim.sulejmani@gmail.com>
2023-12-27 19:44:43 +0100

    List Components

---
+++ js/report-cases/components/list.component.js
@@ -0,0 +1,60 @@
+function ListReportCases() {
+    const self = this;
+
+    self.reports = [];
+
+    self.findAll = async function () {
+        const apiReport = new ApiReportCase();
+        self.reports = await apiReport.findAll();
+    }
+
+    self.findAll();
+
+    return `<div class="table-responsive small">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Reporter</th>
+                <th scope="col">Case Type</th>
+                <th scope="col">Report Data</th>
+                <th scope="col">Description</th>
+                <th scope="col">Address</th>
+                <th scope="col">(action)</th>
+            </tr>
+            </thead>
+            <tbody id="tableReportBody" :loop="self.reports">
+            <tr>
+                <td>{{self.id}}</td>
+                <td>{{self.reporter}}</td>
+                <td>{{self.caseType}}</td>
+                <td>{{self.date}}</td>
+                <td>{{self.description}}</td>
+                <td>{{self.address}}</td>
+                <td>
+                   {{self.active}}
+                </td>
+                <td>
+                    <a href="./print.html?reportCaseId={{self.id}}" class="btn btn-light">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
+                          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                        </svg>
+                    </a>
+                    <a href="./edit.html?reportCaseId={{self.id}}" class="btn btn-warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>
+                    </a>
+                    <a href="./delete.html?reportCaseId={{self.id}}" class="btn btn-danger">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
+                          <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
+                        </svg>
+                    </a>
+                </td>
+             </tr>
+            </tbody>
+        </table>
+    </div>`
+}
